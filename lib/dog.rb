81eb5cb0637916158b5b94a5ea889e5128262ce5
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-05-22 17:09:22 -0400

    commit tree 46d51d9db081af080c251958b949ced029779897

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    attr_accessor :name, :breed, :id
+    def initialize (id, name, breed}
+        @name = name
+        @breed = breed
+    end
 
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+        SQL
+
+        DB[:conn].execute(sql)
+    end
 end
